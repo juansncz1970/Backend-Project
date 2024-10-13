dc9090c2220274b9d752fb5e28d09d015d8632b8
juansncz1970 <jfvolante@gbox.ncf.edu.ph>
2024-10-13 13:28:59 +0800

    Updated README and added remoteDB.sql

---
+++ remoteDB.sql
@@ -0,0 +1,68 @@
+CREATE DATABASE sql12736663;
+
+USE sql12736663;
+
+SHOW TABLES;
+
+CREATE TABLE users (
+    user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fullname VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL UNIQUE KEY,
+    password VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE departments (
+	dept_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    dept_code VARCHAR(255) NOT NULL,
+    dept_name VARCHAR(255) NOT NULL,
+	user_id INT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY AUTO_INCREMENT,
+    course_name VARCHAR(255),
+    course_code VARCHAR(255),
+    user_id INT,
+    dept_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users,
+    FOREIGN KEY (dept_id) REFERENCES departments(dept_id)
+);
+
+CREATE TABLE students (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    lname VARCHAR(255),
+    fname VARCHAR(255),
+    mname VARCHAR(255),
+    user_id INT,
+    course_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users,
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
+
+
+
+SELECT * FROM users;
+SELECT * FROM departments;
+SELECT * FROM courses;
+SELECT * FROM students;
+
+DESC users;
+DESC departments;
+DESC courses;
+DESC students;
+
+drop table users;
+drop table departments;
+drop table courses;
+drop table students;
+
+drop database sql12736663;
